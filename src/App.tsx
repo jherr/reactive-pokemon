11d1a3c5f8a60c4031773b59d22fa007f28b29ee
Jack Herrington <jherr@pobox.com>
2021-07-10 09:03:48 -0700

    Small updates

--- src/App.tsx
+++ src/App.tsx
@@ -16,25 +16,29 @@ const PokemonTable: React.FunctionComponent<{
 }> = ({ pokemon }) => {
   return (
     <table>
-      <tr>
-        <td>ID</td>
-        <td>Name</td>
-        <td>Type</td>
-        <td colSpan={6}>Stats</td>
-      </tr>
-      {pokemon.map((p) => (
-        <tr key={p.id}>
-          <td>{p.id}</td>
-          <td>{p.name}</td>
-          <td>{p.type.join(",")}</td>
-          <td>{p.hp}</td>
-          <td>{p.attack}</td>
-          <td>{p.defense}</td>
-          <td>{p.special_attack}</td>
-          <td>{p.special_defense}</td>
-          <td>{p.speed}</td>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Type</td>
+          <td colSpan={6}>Stats</td>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {pokemon.map((p) => (
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.name}</td>
+            <td>{p.type.join(",")}</td>
+            <td>{p.hp}</td>
+            <td>{p.attack}</td>
+            <td>{p.defense}</td>
+            <td>{p.special_attack}</td>
+            <td>{p.special_defense}</td>
+            <td>{p.speed}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
